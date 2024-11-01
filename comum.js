d9e709ee16c30c8f90eaa53b5832a515b5ad3535
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-11-01 08:33:41 -0300

    Update comum.js

--- comum.js
+++ comum.js
@@ -1,11 +1,13 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel);
 }
+
 const tickfont = {
-    color: getCSS('--cor-primaria'),
+    color:getCSS('--cor-primaria'),
     size: 16,
-    family: gitCSS(' --fonte')
+    family: getCSS('--fonte'),
 }
+
 export{
-    getCSS, tickfont
+    getCSS, tickfont,
 }
